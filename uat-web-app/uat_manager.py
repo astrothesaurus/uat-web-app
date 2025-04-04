5ae3016519221693da926e52bc788aad3953bd68
stevenswait <sswaitUS@users.noreply.github.com>
2025-04-04 11:35:35 -0400

    Simplifying the uat manager for readability

--- uat-web-app/uat_manager.py
+++ uat-web-app/uat_manager.py
@@ -7,18 +7,28 @@ from flask import abort
 
 from data_generator import build_html_list
 
-
-def get_latest_uat_tag():
-    url = os.getenv("UAT_API_URL", "https://api.github.com/repos/astrothesaurus/UAT/releases/latest")
+def fetch_url(url, error_message):
     response = requests.get(url)
     if response.status_code == 200:
-        latest_release = response.json()
-        return latest_release.get("tag_name")
+        return response.json()
     else:
-        error_message = "Failed to fetch the UAT latest release version from github " + url
         logging.error(error_message)
         abort(response.status_code, description=error_message)
 
+def get_latest_uat_tag():
+    url = os.getenv("UAT_API_URL", "https://api.github.com/repos/astrothesaurus/UAT/releases/latest")
+    error_message = "Failed to fetch the UAT latest release version from github " + url
+    return fetch_url(url, error_message).get("tag_name")
+
+def build_html_tree(hierarchy_data):
+    html_tree_parts = ["<ul id='treemenu1' class='treeview'>"]
+    for child in hierarchy_data["children"]:
+        html_tree_parts.append(
+            f"\n\t<li><a id=li-{child['uri'][30:]} href={child['uri'][30:]}?view=hierarchy>{child['name']}</a>")
+        html_tree_parts.append(build_html_list(child, None))
+        html_tree_parts.append("</li>")
+    html_tree_parts.append("\n</ul>")
+    return "".join(html_tree_parts)
 
 class UATManager:
     def __init__(self):
@@ -31,21 +41,15 @@ class UATManager:
     def get_latest_uat_file(self, file_name):
         root_url = os.getenv("UAT_RAW_URL", "https://raw.githubusercontent.com/astrothesaurus/UAT/")
         if not root_url.endswith("/"):
-            root_url = root_url + "/"
+            root_url += "/"
         download_url = root_url + self.tag + "/" + file_name
-        response = requests.get(download_url)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            error_message = "Failed to download the latest UAT file from github " + download_url
-            logging.error(error_message)
-            abort(response.status_code, description=error_message)
+        error_message = "Failed to download the latest UAT file from GitHub " + download_url
+        return fetch_url(download_url, error_message)
 
     def check_uat_version(self):
         new_tag = get_latest_uat_tag()
         if self.tag is None:
             self.update_uat_version()
-
         return {"old_tag": self.tag, "new_tag": new_tag, "is_latest": self.tag == new_tag}
 
     def update_uat_version(self):
@@ -56,23 +60,10 @@ class UATManager:
                 tag_data = self.check_uat_version()
                 if tag_data["is_latest"]:
                     return {"status": "success", "tag": self.tag}
-                else:
-                    self.tag = tag_data["new_tag"]
+                self.tag = tag_data["new_tag"]
 
-            json_data = self.get_latest_uat_file("UAT_list.json")
+            self.alpha_terms = sorted(self.get_latest_uat_file("UAT_list.json"), key=lambda k: k["name"])
             hierarchy_data = self.get_latest_uat_file("UAT.json")
-
-            html_tree_parts = ["<ul id='treemenu1' class='treeview'>"]
-
-            for child in hierarchy_data["children"]:
-                html_tree_parts.append(
-                    f"\n\t<li><a id=li-{child['uri'][30:]} href={child['uri'][30:]}?view=hierarchy>{child['name']}</a>")
-                html_tree_parts.append(build_html_list(child, None))
-                html_tree_parts.append("</li>")
-
-            html_tree_parts.append("\n</ul>")
-
-            self.alpha_terms = sorted(json_data, key=lambda k: k["name"])
-            self.html_tree = "".join(html_tree_parts)
+            self.html_tree = build_html_tree(hierarchy_data)
 
             return {"status": "success", "tag": self.tag}

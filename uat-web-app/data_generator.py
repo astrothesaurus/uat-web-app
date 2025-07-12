e6c99049dc8b2bcbd3d294d720457c21a15aba17
stevenswait <sswaitUS@users.noreply.github.com>
2025-07-12 09:41:15 -0400

    Adding space to comment to fix linting issue

--- uat-web-app/data_generator.py
+++ uat-web-app/data_generator.py
@@ -135,7 +135,7 @@ def search_terms(lookup_term, alpha_terms, sort_direction="alpha"):
 
     if sort_direction == "relevance":
         results.sort(key=lambda x: (x["_rank"], x["name"].lower()))
-    for result in results: # Remove the _rank key from the result
+    for result in results:  # Remove the _rank key from the result
         result.pop("_rank", None)
     return results
 

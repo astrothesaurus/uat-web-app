45cab3be345aa752c8b5334cabf04edb0e57af6e
stevenswait <sswaitUS@users.noreply.github.com>
2025-08-20 17:50:26 -0400

    Adding gmail and outlook browser email client options for users

--- uat-web-app/static/js/sorting/formOperations.js
+++ uat-web-app/static/js/sorting/formOperations.js
@@ -1,6 +1,9 @@
 /**
  * Sends form data and tree differences via an HTTP POST request.
  */
+const email = "uat-curation@googlegroups.com";
+const subject = encodeURIComponent("Suggestions from Sorting Tool");
+
 function TestForm() {
     let root = getRoot();
     let orig = getOrig();
@@ -8,12 +11,11 @@ function TestForm() {
     let name = document.getElementById("first_name").value;
     let inst = document.getElementById("yourinst").value;
     let urnotes = document.getElementById("notes").value;
-    let subject = encodeURIComponent("Suggestions from Sorting Tool");
     let body = encodeURIComponent(
         `Name: ${name}\nInstitution: ${inst}\nNotes: ${urnotes}\n\nDifference File:\n${diffStr}`
     );
-    const email = "uat-curation@googlegroups.com";
     window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+    return body;
 }
 
 /**
@@ -31,8 +33,13 @@ function checkform(theform) {
         }
         if (theform.txtInput.value !== "") {
             if (ValidCaptcha()) {
-                why += "Thank you for your feedback!<br>Your email client should have opened.<br>Please review and send the email to complete your submission.";
-                TestForm();
+                let body = TestForm();
+                const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
+                const outlookUrl = `https://outlook.office.com/mail/deeplink/compose?to=${email}&subject=${subject}&body=${body}`;
+                why += `Thank you for your feedback!<br>Your email client should have opened.<br>Please review and send the email to complete your submission.<br>
+                Other Email options:<br>
+                <button type="button" class="btn btn-danger" onclick="window.open('${gmailUrl}', '_blank')">Gmail</button>
+                <button type="button" class="btn btn-primary" style="background-color:#0072C6; border-color:#0072C6;" onclick="window.open('${outlookUrl}', '_blank')">Outlook 365</button>`;
             } else {
                 why += "Robot Check code did not match.";
             }
